5feb47291ced121048bb6e748737b611a67d2520
hernc <hernc820@newschool.edu>
2016-03-03 15:41:36 -0500

    added image

---
+++ sketch_basic/sketch3.js
@@ -0,0 +1,23 @@
+// a simple p5 sketch that draws an image
+
+var img;
+
+// images must be loaded before they can be used
+// use the preload() function to load your images and p5 will make sure they
+// are loaded before setup() and draw() are called
+// note that in your sketches, your load image probably won't require a path.
+// you will use something like loadImage("shell.jpg")
+
+function preload() {
+ 	img = loadImage("lion.jpg");
+}
+
+function setup() {
+	createCanvas(600, 600);
+	imageMode(CENTER);
+}
+
+function draw() {
+	background(125, 125, 125);
+	// image(img, mouseX, mouseY);
+}
